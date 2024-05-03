f4b70bfb20d45c8d0d5c7736bcb9c57934c5d4f1
unknown <tigermask178@gmail.com>
2024-05-04 02:13:13 +0800

    teshub

--- hub2.lua
+++ hub2.lua
@@ -1,13 +1,5 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
-    if game.PlaceId == 12978425304 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pryxo/Hub/main/Toy%20Simulator"))()
-    elseif game.PlaceId == 13680982680 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pryxo/Hub/main/Strength%20Simulator"))()
-    elseif game.PlaceId == 11497091217 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pryxo/Hub/main/Popping%20Simulator!"))()
-    elseif game.PlaceId == 14171094615 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pryxo/Hub/main/Clicker%20Wars%20Simulator!"))()
-    elseif game.PlaceId == 11827760850 then
+    if game.PlaceId == 11827760850 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/11827760850"))()
     elseif game.PlaceId == 15025029467 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/15025029467"))()
