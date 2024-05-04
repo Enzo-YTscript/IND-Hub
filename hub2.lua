55bf70cf0e0cd12dae130eaf197cf2249a1d8496
unknown <tigermask178@gmail.com>
2024-05-05 01:34:03 +0800

    Hub

--- hub2.lua
+++
@@ -1,8 +0,0 @@
-if not game:IsLoaded() then game.Loaded:Wait() end
-    if game.PlaceId == 11827760850 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/11827760850"))()
-    elseif game.PlaceId == 15025029467 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/15025029467"))()
-    else
-      print("Game isn't Supported")
-end
