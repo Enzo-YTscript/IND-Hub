d53120cf15deeac8b515364e733225c74b8a738b
unknown <tigermask178@gmail.com>
2024-07-10 02:44:11 +0800

    hub2tes

---
+++ hub2.lua
@@ -0,0 +1,20 @@
+
+if not game:IsLoaded() then game.Loaded:Wait() end
+    if game.PlaceId == 12978425304 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pryxo/Hub/main/Toy%20Simulator"))()
+    elseif game.PlaceId == 13680982680 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pryxo/Hub/main/Strength%20Simulator"))()
+    elseif game.PlaceId == 11497091217 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pryxo/Hub/main/Popping%20Simulator!"))()
+    elseif game.PlaceId == 14171094615 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pryxo/Hub/main/Clicker%20Wars%20Simulator!"))()
+    elseif game.PlaceId == 13891637445 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pryxo/Hub/main/Lucky%20Block%20Legends!"))()
+    elseif game.PlaceId == 15025029467 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Enzo-YTscript/scrp/blob/main/15025029467.lua"))()
+    else
+    elseif game.PlaceId == 13127800756 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/13127800756.lua"))()
+    else
+      print("Game isn't Supported")
+end
