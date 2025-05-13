465882409f04ed413ca6f658dec9aaade9c762a0
Enzo-YTscript <email-akun-tigermask178@gmail.com>
2025-05-13 20:35:15 +0800

    upd

---
+++ INDHub.lua
@@ -0,0 +1,21 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+
+local scripts = {
+    [120570155878200] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/120570155878200.lua", -- Broom Training
+    [13127800756] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/13127800756.lua",
+    [11827760850] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/11827760850.lua",
+    [12978425304] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/12978425304.lua",
+    [15139160794] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/15139160794.lua",
+    [18629053284] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/18629053284.lua", -- Survival Odyssey
+    [18629058177] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/18629058177.lua", -- Void
+    [92039548740735] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/92039548740735.lua", -- Underworld
+    [93787311916283] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/93787311916283.lua", -- Horse Race
+    [125415045928192] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/125415045928192.lua" -- Roller Training
+}
+
+local url = scripts[game.PlaceId]
+if url then
+    pcall(loadstring(game:HttpGet(url)))
+else
+    print("Game isn't Supported")
+end
