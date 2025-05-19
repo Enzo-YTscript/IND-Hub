c585513410637c17ab3797a3207a0e685cfb9eee
Enzo-YTscript <tigermask178@gmail.com>
2025-05-20 04:00:18 +0800

    updt

--- INDHub.lua
+++ INDHub.lua
@@ -14,6 +14,8 @@ local scripts = {
     [93787311916283] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/93787311916283.lua", -- Horse Race
     [125415045928192] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/125415045928192.lua", -- Roller Training
     [112678615086652] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/112678615086652.lua", -- Ride Race
+	[101645554076038] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/101645554076038.lua", -- Paper Plane sim
+	
 }
 
 local url = scripts[game.PlaceId]
