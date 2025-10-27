27bf0bc36357ce6d3a6cb06c83b13b3a329f0a42
Enzo-YTscript <tigermask178@gmail.com>
2025-10-28 03:30:57 +0800

    updt

--- INDHub.lua
+++ INDHub.lua
@@ -21,6 +21,7 @@ local scripts = {
 	[81257648942232] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/81257648942232.lua", -- Slash Mob Simulator
 	[125661369726244] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/125661369726244.lua", -- Anime Apex
 	[16713816479] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/16713816479.lua", -- Pet Quest
+	[73823535458649] = "https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/73823535458649.lua", -- Pet Evolution Incremental
 	
 }
 
